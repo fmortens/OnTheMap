db43159b01f4a06ec7fa2b396e7a796e7f2802b5
Frank Mortensen <fmortens@me.com>
2019-02-08 21:43:02 +0100

    Set up table view as well

--- OnTheMap/Controller/TableViewController.swift
+++ OnTheMap/Controller/TableViewController.swift
@@ -8,16 +8,102 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        
+        refreshControl.tintColor = .red
+        
+        refreshControl.addTarget(
+            self,
+            action: #selector(requestData),
+            for: .valueChanged
+        )
+        
+        return refreshControl
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("TABLE VIEW CONTROLLER IS ALIVE")
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = refreshControl
+        } else {
+            tableView.addSubview(refreshControl)
+        }
+        
+        requestData()
         
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
+    @objc
+    func requestData() {
+        ParseClient.loadStudentLocations(completion: handleStudentLocationsResult)
+    }
+    
+    func handleStudentLocationsResult(success: Bool, error: Error?) {
+        let deadline = DispatchTime.now() + .milliseconds(500)
+        DispatchQueue.main.asyncAfter(deadline: deadline, execute: {
+            self.refreshControl.endRefreshing()
+        })
+        
+        self.tableView.reloadData()
+    }
+    
+}
+
+extension TableViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataModel.studentInformationList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StudentInformationTableViewCell")!
+        
+        let studentInformation = DataModel.studentInformationList[indexPath.row]
+        
+        cell.textLabel?.text = "\(studentInformation.firstName ?? "") \(studentInformation.lastName ?? "")"
+        //cell.imageView?.image = UIImage(named: "PosterPlaceholder")
+        
+//        if let posterPath = movie.posterPath {
+//            TMDBClient.downloadPosterImage(posterPath: posterPath) { (data, error) in
+//                guard let data = data else {
+//                    return
+//                }
+//
+//                let image = UIImage(data: data)
+//                cell.imageView?.image = image
+//                cell.setNeedsLayout()
+//            }
+//        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        selectedIndex = indexPath.row
+//        performSegue(withIdentifier: "showDetail", sender: nil)
+        
+        print("row \(indexPath.row) was tapped")
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }
