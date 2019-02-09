90e91271943b86494a9c8a63e1915e91fe6f33b3
Frank Mortensen <fmortens@me.com>
2019-02-09 23:16:30 +0100

    Add outlets for text fields and present alert on error, note we are presenting from a modal

--- OnTheMap/Controller/InformationPostingViewController.swift
+++ OnTheMap/Controller/InformationPostingViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import CoreLocation
 
 class InformationPostingViewController: UIViewController {
+    
+    @IBOutlet weak var locationTextInput: UITextField!
+    @IBOutlet weak var linkTextInput: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,11 +22,48 @@ class InformationPostingViewController: UIViewController {
         print("addLocationViewController")
     }
     
+    func handleGeocodeAddress(placeMark: [CLPlacemark]?, error: Error?) {
+        guard let placeMark = placeMark else {
+            print("COULD NOT RESOLVE LOCATION!")
+            let alertVC = UIAlertController(title: "Error", message: "Could not resolve location!", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+            
+            return
+        }
+        
+        print("\(String(describing: placeMark))")
+    }
     
     @IBAction func didTapCancelButton(_ sender: Any) {
-        
+    
         self.dismiss(animated: true, completion: nil)
         
     }
     
+    @IBAction func didTapFindLocationButton(_ sender: Any) {
+        
+        if let addressString = self.locationTextInput.text,
+           let linkText = self.linkTextInput.text {
+            
+            print("FIND LOCATION")
+            
+            let errorMessage = addressString == "" ? "Missing address" : linkText == "" ? "Missing link" : ""
+            
+            if errorMessage != "" {
+                print("Something is wrong")
+                
+                
+                let alertVC = UIAlertController(title: "Error", message: errorMessage, preferredStyle: .alert)
+                alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alertVC, animated: true, completion: nil)
+                
+                
+            } else {
+                print("we are good")
+                let geocoder = CLGeocoder()
+                geocoder.geocodeAddressString(addressString, completionHandler: handleGeocodeAddress(placeMark:error:))
+            }
+        }
+    }
 }
